81cb699e28bfe5aee32c68d6d4d9ea20d34344b5
WorkStation <XaJIaC@example.com>
2023-12-12 13:02:57 +0300

    Почти сделан 1200

---
+++ src/js/slider.js
@@ -0,0 +1,39 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   const portfolio = new Swiper('#main-slider', {
+//     slidesPerView: 1,
+//     //slidesPerView: 14,
+//     //  centeredSlides: true,
+//     // slidesPerView: 14,
+//     lazy: true,
+//     loop: true,
+//     clickable: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     //Pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+
+//     breakpoints: {
+//       1200: {
+//     //   slidesPerView:14,
+//         spaceBetween: 15
+//       },
+//       900: {
+//     //        slidesPerView:9.5,
+//         spaceBetween: 15
+//       },
+//       540: {
+//     //   slidesPerView: 4.5,
+//         spaceBetween: 5
+//       },
+//       440: {
+//     //   slidesPerView:3.5,
+//         spaceBetween: 5
+//       },
+//     }
+//   });
+// });
